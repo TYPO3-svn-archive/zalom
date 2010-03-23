a2c498db5c36ed24d59d2fa284a35ceb10b7acac
kiddog <kiddog@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-23 09:53:05 +0000

    Aufruf von "wget" mit dem Parameter --level=0 erweitert, da der Download ab einer Gewissen Level-Stufe endete.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/zalom/trunk@31371 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/ShellScripts/getData.sh
+++ Resources/ShellScripts/getData.sh
@@ -6,10 +6,10 @@ mv typo3conf original_typo3conf
 mv typo3temp original_typo3temp
 mv uploads original_uploads
 
-wget -r -c ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/fileadmin
-wget -r -c ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3conf
-wget -r -c ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3temp
-wget -r -c ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/uploads
+wget -r -c --level=0 ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/fileadmin
+wget -r -c --level=0 ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3conf
+wget -r -c --level=0 ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3temp
+wget -r -c --level=0 ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/uploads
 
 mv $ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3conf/localconf.php $ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3conf/localconf_original.php
 mv $ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/* ./
