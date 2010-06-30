e54b1bb07997a367bd6425dd04944fe5de3203aa
kiddog <kiddog@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-30 13:49:22 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/zalom/branches/ferrari@35129 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/ShellScripts/getDataForLocalExtensionInstallation.sh
+++ Resources/ShellScripts/getDataForLocalExtensionInstallation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source config.sh
 
-wget -r -c ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3/ext
+wget -r -c --level=0 ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3/ext
 mv $ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3/ext/* ./typo3conf/ext/
 rm -r $ZALOM_FTP_URL
