cf0cf33d7f1344354e84792fe553297cdc0ae989
webtekk <webtekk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-11 12:59:39 +0000

    Task #11896: Installationsprozess optimieren Um ZALOM einfacher auf einem externen Server zu installieren, wurde diverse Anpassungen an den Skripten und der Dokumentation vorgenommen.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/zalom/trunk@41999 735d13b6-9817-0410-8766-e36946ffe9aa

--- resources/shell/clearInstallData.sh
+++ resources/shell/clearInstallData.sh
@@ -6,5 +6,12 @@ rm -r original_typo3conf
 rm -r original_typo3temp
 rm -r original_uploads
 
+rm -r typo3conf/ext/zalom/log
+rm -r typo3conf/ext/zalom/resources/backup
+rm -r typo3conf/ext/zalom/resources/documents
+rm -r typo3conf/ext/zalom/resources/msd
+rm -r typo3conf/ext/zalom/resources/mysqldump
+rm -r typo3conf/ext/zalom/resources/shell
+
 rm getData.sh
 rm config.sh
--- resources/shell/getData.sh
+++ resources/shell/getData.sh
@@ -11,15 +11,16 @@ wget -r -c --level=0 ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$Z
 wget -r -c --level=0 ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3temp
 wget -r -c --level=0 ftp://$ZALOM_FTP_USER:$ZALOM_FTP_PASSWORD@$ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/uploads
 
-mv $ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3conf/localconf.php $ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3conf/localconf_original.php
+#mv $ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3conf/localconf.php $ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/typo3conf/localconf_original.php
 mv $ZALOM_FTP_URL/$ZALOM_FTP_WWWROOT/* ./
 
 # Original localconf.php ins aktuelle typo3conf-Verzeichniss kopieren.
-cp original_typo3conf/localconf.php typo3conf/localconf.php
+#cp original_typo3conf/localconf.php typo3conf/localconf.php
 
 # mysql-Dump kopieren
-cp typo3conf/ext/zalom/resources/mysqldump/$DUMP_FILE typo3conf/$DUMP_FILE
- 
+#cp typo3conf/ext/zalom/resources/mysqldump/$DUMP_FILE typo3conf/$DUMP_FILE
+
+# temporaere Daten loeschen
 rm -r $ZALOM_FTP_URL
 rm typo3conf/temp_CACHED_*.php
 
